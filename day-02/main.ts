031bf93937dde3df34bfceab38fa86ed3fde86ed
gallejens <galle.jens@telenet.be>
2023-12-08 15:28:30 +0100

    feat(day-02): finish part 2

--- day-02/main.ts
+++ day-02/main.ts
@@ -1,48 +1,89 @@
 const data = await Deno.readTextFile('day-02/data.txt');
 const lines = data.split('\r\n');
 
-const MAX: Record<string, number> = {
-  red: 12,
-  green: 13,
-  blue: 14,
-};
+const solvePartOne = () => {
+  console.time('Part one');
 
-let total = 0;
+  const MAX: Record<string, number> = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  };
 
-const isSetPossible = (set: string) => {
-  const hands = set.split(',');
+  let total = 0;
 
-  for (const hand of hands) {
-    const [amount, color] = hand.trim().split(' ');
-    if (+amount > MAX[color]) {
-      return false;
+  const isSetPossible = (set: string) => {
+    const hands = set.split(',');
+
+    for (const hand of hands) {
+      const [amount, color] = hand.trim().split(' ');
+      if (+amount > MAX[color]) {
+        return false;
+      }
     }
+
+    return true;
+  };
+
+  for (const line of lines) {
+    const splitLine = line.split(':');
+    if (splitLine.length !== 2)
+      throw new Error(`More than one colon in line ${line}`);
+
+    const id = +splitLine[0].replace('Game ', '');
+    const sets = splitLine[1].trim().split(';');
+
+    let valid = true;
+    for (const set of sets) {
+      const setPossible = isSetPossible(set);
+      if (setPossible) continue;
+
+      valid = false;
+      break;
+    }
+
+    if (!valid) continue;
+
+    total += id;
   }
 
-  return true;
+  console.timeEnd('Part one');
+  console.log(`Solution part one: ${total}`);
 };
 
-for (const line of lines) {
-  const splitLine = line.split(':');
-  if (splitLine.length !== 2)
-    throw new Error(`More than one colon in line ${line}`);
+const solvePartTwo = () => {
+  console.time('Part two');
 
-  const id = +splitLine[0].replace('Game ', '');
-  const sets = splitLine[1].trim().split(';');
+  let totalPower = 0;
 
-  let valid = true;
-  for (const set of sets) {
-    const setPossible = isSetPossible(set);
-    console.log(`${set} is ${setPossible ? 'possible' : 'not possible'}`);
-    if (setPossible) continue;
+  for (const line of lines) {
+    const sets = line.split(':')[1].trim().split(';');
 
-    valid = false;
-    break;
+    const highestAmounts: Record<string, number> = {};
+
+    for (const set of sets) {
+      const hands = set.split(',');
+
+      for (const hand of hands) {
+        const [amountStr, color] = hand.trim().split(' ');
+        const amount = +amountStr;
+
+        if (!highestAmounts[color] || amount > highestAmounts[color]) {
+          highestAmounts[color] = amount;
+          continue;
+        }
+      }
+    }
+
+    totalPower += Object.values(highestAmounts).reduce(
+      (acc, cur) => acc * cur,
+      1
+    );
   }
 
-  if (!valid) continue;
+  console.timeEnd('Part two');
+  console.log(`Solution part two: ${totalPower}`);
+};
 
-  total += id;
-}
-
-console.log(total);
+solvePartOne();
+solvePartTwo();
