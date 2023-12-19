7696bcaad5173f27f120a4c7c77a4dfa4c165145
gallejens <galle.jens@telenet.be>
2023-12-19 10:22:53 +0100

    feat(day-06): solve part two

--- day-06/main.ts
+++ day-06/main.ts
@@ -5,12 +5,14 @@ const allLineNumbers = data
   .filter(x => x !== null)
   .map(x => x!.map(y => parseInt(y)));
 
-const races: { time: number; distance: number }[] = [];
-for (let i = 0; i < allLineNumbers[0]?.length; i++) {
-  races.push({ time: allLineNumbers[0][i], distance: allLineNumbers[1][i] });
-}
+type Race = { time: number; distance: number };
 
 const solvePartOne = () => {
+  const races: Race[] = [];
+  for (let i = 0; i < allLineNumbers[0]?.length; i++) {
+    races.push({ time: allLineNumbers[0][i], distance: allLineNumbers[1][i] });
+  }
+
   let solution = 1;
 
   for (const race of races) {
@@ -27,7 +29,25 @@ const solvePartOne = () => {
   console.log(`Solution part one: ${solution}`);
 };
 
-const solvePartTwo = () => {};
+const solvePartTwo = () => {
+  const parseNumbers = (line: number[]) =>
+    parseInt(line.reduce<string>((a, b) => `${a}${b}`, ''));
+
+  const race: Race = {
+    time: parseNumbers(allLineNumbers[0]),
+    distance: parseNumbers(allLineNumbers[1]),
+  };
+
+  let possible = 0;
+  for (let i = 1; i < race.time; i++) {
+    const distance = i * (race.time - i);
+    if (distance > race.distance) {
+      possible++;
+    }
+  }
+
+  console.log(`Solution part two: ${possible}`);
+};
 
 solvePartOne();
 solvePartTwo();
